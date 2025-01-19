53c359324b10a4a4af5a112f8ad7380f42543414
KorrumzPL <korrumz2@gmail.com>
2025-01-19 14:21:42 +0100

    refactor: rename button styles to forms, adjust layout for better spacing

--- src/dashboard/guild/[id]/custombots/page.tsx
+++ src/dashboard/guild/[id]/custombots/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../../../../components/ui/navigation/navbar";
 import Sidebar from "../../../../components/ui/navigation/sidebar";
-import InsertSetting from "../../../../components/ui/buttons/add-custombot";
+import InsertSetting from "../../../../components/ui/forms/adding-custombot";
 
 export default function Page() {
 	return (
