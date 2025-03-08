0fb607194f02d23d28ecf18b04bbf467c24a153b
KorrumzPL <korrumz2@gmail.com>
2025-03-08 23:43:51 +0100

    fix: "Internet disconnected" error

--- src/guilds-list.tsx
+++ src/guilds-list.tsx
@@ -6,56 +6,55 @@ import { fetchGuilds } from "./thunks/guilds";
 import { fetchUser } from "./thunks/user";
 
 interface User {
-	username: string;
+    username: string;
 }
 
 export default function GuildsList() {
-	const dispatch = useDispatch<AppDispatch>();
-	const guilds = useSelector((state: RootState) => state.guilds.data);
-	const user = useSelector(
-		(state: RootState) => state.user.data as unknown as User,
-	);
+    const dispatch = useDispatch<AppDispatch>();
+    const guilds = useSelector((state: RootState) => state.guilds.data);
+    const user = useSelector(
+        (state: RootState) => state.user.data as unknown as User,
+    );
 
-	useEffect(() => {
-		dispatch(fetchGuilds());
-		dispatch(fetchUser());
-	}, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchGuilds());
+        dispatch(fetchUser());
+    }, [dispatch]);
 
-	return (
-		<div>
-			<h1 className="title"> Welcome, {user?.username} </h1>
-			<h2 className="subtitle"> Please, choose guild:</h2>
-			<div className="guilds-container">
-				<div className="guilds-grid">
-					{guilds.map((guild) => (
-						<div className="guild">
-							<img
-								src={
-									guild?.icon
-										? `https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`
-										: "/default_guild_icon.png"
-								}
-								alt={`<no icon>`}
-								onError={(e) => {
-									e.currentTarget.src =
-										"/default_guild_icon.png";
-								}}
-							/>
-							<p className="guild-name">{guild?.name}</p>
-							<button className="guild-button">
-								<Link
-									key={guild?.id}
-									to={`/dashboard/guild/${guild?.id}/home`}
-									className="guild-button"
-								>
-									<img src="/gear.svg" alt="gear icon" />
-									SET
-								</Link>
-							</button>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <h1 className="title"> Welcome, {user?.username} </h1>
+            <h2 className="subtitle"> Please, choose guild:</h2>
+            <div className="guilds-container">
+                <div className="guilds-grid">
+                    {guilds.map((guild) => (
+                        <div className="guild" key={guild?.id}>
+                            <img
+                                src={
+                                    guild?.icon
+                                        ? `https://cdn.discordapp.com/icons/${guild?.id}/${guild?.icon}.png`
+                                        : "/default_guild_icon.png"
+                                }
+                                alt={`<no icon>`}
+                                onError={(e) => {
+                                    e.currentTarget.src =
+                                        "/default_guild_icon.png";
+                                }}
+                            />
+                            <p className="guild-name">{guild?.name}</p>
+                            <button className="guild-button">
+                                <Link
+                                    to={`/dashboard/guild/${guild?.id}/home`}
+                                    className="guild-button"
+                                >
+                                    <img src="/gear.svg" alt="gear icon" />
+                                    SET
+                                </Link>
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
