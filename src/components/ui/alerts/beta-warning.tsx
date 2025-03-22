f817c4e312d4c69e72078ce1b14ce23c48c241a6
KorrumzPL <korrumz2@gmail.com>
2025-03-22 15:28:32 +0100

    fix: polish text width alerts

--- src/components/ui/alerts/beta-warning.tsx
+++ src/components/ui/alerts/beta-warning.tsx
@@ -4,15 +4,17 @@ export default function BetaWarning() {
 	const { t } = useTranslation();
 	return (
 		<div>
-						<img
-				src="/warning-icon.svg"
-				alt="warning"
-				className={classes.betaWarningOverlayIcon}
-			/>
-			<div className={classes.betaWarningOverlay}>
-				<p className={classes.betaWarningOverlayText}>
-					{t("system.alerts.beta_warning")}
-				</p>
+			<div className={classes.betaWarning}>
+				<div className={classes.betaWarningOverlay}>
+				<img
+					src="/warning-icon.svg"
+					alt="warning"
+					className={classes.betaWarningOverlayIcon}
+				/>
+					<p className={classes.betaWarningOverlayText}>
+						{t("system.alerts.beta_warning")}
+					</p>
+				</div>
 			</div>
 		</div>
 	);
