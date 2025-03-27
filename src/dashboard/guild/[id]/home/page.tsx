a3c198bc30fb5f3724bd45e912e521fbf130de04
KorrumzPL <korrumz2@gmail.com>
2025-03-27 19:39:19 +0100

    feat: home main settings

--- src/dashboard/guild/[id]/home/page.tsx
+++ src/dashboard/guild/[id]/home/page.tsx
@@ -8,6 +8,51 @@ import { SelectOption } from "@/components/ui/inputs/search";
 import { useSelector } from "react-redux";
 import type { RootState } from "@/store";
 import { useState } from "react";
+interface SettingContainerProps {
+	title: string;
+	children?: React.ReactNode;
+}
+interface SettingInputProps {
+	title: string;
+	options: { id: string; name: string }[];
+	searchTerm: string;
+	setSearchTerm: (value: string) => void;
+	onChange: (value: string) => void;
+}
+const SettingContainer = ({
+	title,
+	children,
+}: SettingContainerProps & { children?: React.ReactNode }) => (
+	<div className={classes.container}>
+		<p className={classes.containerTitle}>{title}</p>
+		{children}
+	</div>
+);
+const SettingInput = ({
+	title,
+	options,
+	searchTerm,
+	setSearchTerm,
+	onChange,
+}: SettingInputProps) => (
+	<div className={classes.inputContainer}>
+		<p className={classes.inputTitle}>{title}</p>
+		<SearchSelect
+			value={searchTerm}
+			onChange={onChange}
+			searchTerm={searchTerm}
+			setSearchTerm={setSearchTerm}
+			inputClassName={classes.searchInput}
+			indicatorClassName={classes.searchIndicator}
+		>
+			{options.map((option) => (
+				<SelectOption key={option.id} value={option.name}>
+					{option.name}
+				</SelectOption>
+			))}
+		</SearchSelect>
+	</div>
+);
 export default function Page() {
 	const [starboardChannelSearchTerm, setStarboardChannelSearchTerm] =
 		useState("");
@@ -68,10 +113,10 @@ export default function Page() {
 			<Sidebar />
 			<BetaWarning />
 			<JoinSupportAlert />
-			<div className={classes.container}>
-				<p className={classes.containerTitle}>
-					Main settings
-					<svg
+
+<p className={classes.sectionTitle}> Main settings 
+
+<svg
 						width="185"
 						height="4"
 						viewBox="0 0 185 4"
@@ -82,134 +127,55 @@ export default function Page() {
 							strokeWidth: 4,
 							stroke: "#275EE7",
 						}}
-						className={classes.titleVector}
+						className={classes.sectionTitleVector}
 					>
 						<line x1="0" y1="2" x2="185" y2="2" />
 					</svg>
-				</p>
-			</div>
-
-			<div className={classes.settingsContainer}>
-				<p className={classes.containerSubtitle}>
-					Starboard
-					<p className={classes.optionTitle}>Starboard channel</p>
-					<img
-						src="/hashtag.svg"
-						alt="hashtag"
-						className={classes.hashtag}
-					/>
-					<SearchSelect
-						value={starboardChannelSearchTerm}
-						onChange={handleStarboardChannelChange}
-						searchTerm={starboardChannelSearchTerm}
-						setSearchTerm={setStarboardChannelSearchTerm}
-						inputClassName={classes.searchInput}
-						indicatorClassName={classes.searchIndicator}
-					>
-						{filteredChannels(starboardChannelSearchTerm).map(
-							(channel) => (
-								<SelectOption
-									key={channel.id}
-									value={channel.name}
-								>
-									{channel.name}
-								</SelectOption>
-							),
-						)}
-					</SearchSelect>
-					<p className={classes.optionTitle}>Starboard type</p>
-					<img src="/star.svg" alt="star" className={classes.star} />
-					<SearchSelect
-						value={starboardTypeSearchTerm}
-						onChange={handleStarboardTypeChange}
-						searchTerm={starboardTypeSearchTerm}
-						setSearchTerm={setStarboardTypeSearchTerm}
-						inputClassName={classes.searchInput}
-						indicatorClassName={classes.searchIndicator}
-					>
-						{starboardTypeChoices.map((type) => (
-							<SelectOption key={type.value} value={type.value}>
-								{type.label}
-							</SelectOption>
-						))}
-					</SearchSelect>
-				</p>
-			</div>
-			<div className={classes.settingsContainer}>
-				<p className={classes.containerSubtitle}>
-					Welcoming
-					<p className={classes.optionTitle}>Welcome channel</p>
-					<img
-						src="/hashtag.svg"
-						alt="hashtag"
-						className={classes.hashtag}
-					/>
-					<SearchSelect
-						value={welcomeChannelSearchTerm}
-						onChange={handleWelcomeChannelChange}
-						searchTerm={welcomeChannelSearchTerm}
-						setSearchTerm={setWelcomeChannelSearchTerm}
-						inputClassName={classes.searchInput}
-						indicatorClassName={classes.searchIndicator}
-					>
-						{filteredChannels(welcomeChannelSearchTerm).map(
-							(channel) => (
-								<SelectOption
-									key={channel.id}
-									value={channel.name}
-								>
-									{channel.name}
-								</SelectOption>
-							),
-						)}
-					</SearchSelect>
-					<p className={classes.optionTitle}>Goodbye channel</p>
-					<img
-						src="/hashtag.svg"
-						alt="hashtag"
-						className={classes.hashtag}
-					/>
-					<SearchSelect
-						value={goodbyeChannelSearchTerm}
-						onChange={handleGoodbyeChannelChange}
-						searchTerm={goodbyeChannelSearchTerm}
-						setSearchTerm={setGoodbyeChannelSearchTerm}
-						inputClassName={classes.searchInput}
-						indicatorClassName={classes.searchIndicator}
-					>
-						{filteredChannels(goodbyeChannelSearchTerm).map(
-							(channel) => (
-								<SelectOption
-									key={channel.id}
-									value={channel.name}
-								>
-									{channel.name}
-								</SelectOption>
-							),
-						)}
-					</SearchSelect>
-				</p>
-			</div>
-			<div className={classes.settingsContainer}>
-				<p className={classes.containerSubtitle}>
-					Additional
-					<p className={classes.optionTitle}>Auto role</p>
-					<SearchSelect
-						value={autoRoleSearchTerm}
-						onChange={handleAutoRoleChange}
-						searchTerm={autoRoleSearchTerm}
-						setSearchTerm={setAutoRoleSearchTerm}
-						inputClassName={classes.searchInput}
-						indicatorClassName={classes.searchIndicator}
-					>
-						{filteredRoles(autoRoleSearchTerm).map((role) => (
-							<SelectOption key={role.id} value={role.name}>
-								{role.name}
-							</SelectOption>
-						))}
-					</SearchSelect>
-				</p>
-			</div>
+</p>
+			<SettingContainer title="Starboard">
+				<SettingInput
+					title="Starboard Channel"
+					options={filteredChannels(starboardChannelSearchTerm)}
+					searchTerm={starboardChannelSearchTerm}
+					setSearchTerm={setStarboardChannelSearchTerm}
+					onChange={handleStarboardChannelChange}
+				/>
+				<SettingInput
+					title="Starboard Type"
+					options={starboardTypeChoices.map((choice) => ({
+						id: choice.value,
+						name: choice.label,
+					}))}
+					searchTerm={starboardTypeSearchTerm}
+					setSearchTerm={setStarboardTypeSearchTerm}
+					onChange={handleStarboardTypeChange}
+				/>
+			</SettingContainer>
+			<SettingContainer title="Welcoming">
+				<SettingInput
+					title="Welcome Channel"
+					options={filteredChannels(welcomeChannelSearchTerm)}
+					searchTerm={welcomeChannelSearchTerm}
+					setSearchTerm={setWelcomeChannelSearchTerm}
+					onChange={handleWelcomeChannelChange}
+				/>
+				<SettingInput
+					title="Goodbye Channel"
+					options={filteredChannels(goodbyeChannelSearchTerm)}
+					searchTerm={goodbyeChannelSearchTerm}
+					setSearchTerm={setGoodbyeChannelSearchTerm}
+					onChange={handleGoodbyeChannelChange}
+				/>
+			</SettingContainer>
+			<SettingContainer title="Additionals">
+				<SettingInput
+					title="Auto Role"
+					options={filteredRoles(autoRoleSearchTerm)}
+					searchTerm={autoRoleSearchTerm}
+					setSearchTerm={setAutoRoleSearchTerm}
+					onChange={handleAutoRoleChange}
+				/>
+			</SettingContainer>
 		</div>
 	);
 }
