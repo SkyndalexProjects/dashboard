21809f55b9fc77cd0712bb86b477b96d5066dc41
KorrumzPL <korrumz2@gmail.com>
2025-05-01 18:50:54 +0200

    fix: modal design, add vector animation

--- src/dashboard/guild/[id]/main-settings/page.tsx
+++ src/dashboard/guild/[id]/main-settings/page.tsx
@@ -1,13 +1,20 @@
 import Navbar from "@/components/ui/navigation/navbar";
 import Sidebar from "@/components/ui/navigation/sidebar";
 import classes from "./main-settings.module.css";
+import Switch from "@/components/ui/inputs/switch";
 export default function MainSettings() {
 	return (
 		<div>
 			<Navbar />
 			<Sidebar />
 
-			<h1> Dupa </h1>
+			<p className={classes.greetingsAndFarewell}>
+				{" "}
+				Greetings & Farewell
+				<div className={classes.underlineVector}></div>
+			</p>
+
+			<Switch switchClassName={classes.switch} knobClassName={classes.knob} />
 		</div>
 	);
 }
