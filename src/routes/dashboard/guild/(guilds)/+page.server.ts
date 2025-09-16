93551433f30e2c726fe919ddb108762b13aeef23
KorrumzPL <korrumzprivate@gmail.com>
2025-09-16 21:04:32 +0200

    feat: guild data loading

--- src/routes/dashboard/guild/(guilds)/+page.server.ts
+++ src/routes/dashboard/guild/(guilds)/+page.server.ts
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit';
+import type { APIGuild } from 'discord-api-types/v10';
 
-export async function load({ fetch }) {
+export async function load({ fetch }): Promise<{ guilds: APIGuild[] }> {
 	try {
 		const res = await fetch(`http://localhost:3000/api/guilds`, {
 			credentials: 'include'
@@ -10,10 +11,12 @@ export async function load({ fetch }) {
 			error(res.status, 'Error while fetching guilds');
 		}
 
-		const guildsData = await res.json();
-		return { guilds: guildsData };
+		const guildsData: APIGuild[] = await res.json();
+		return {
+			guilds: guildsData
+		};
 	} catch (err) {
 		console.error('Error while downloading guilds data:', err);
-		return { data: null };
+		return { guilds: [] };
 	}
 }
