6b2ef7f957482b919bdeb80aec778fd40eb60c17
KorrumzPL <korrumzprivate@gmail.com>
2025-11-05 19:28:45 +0100

    test

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,6 @@ import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
 const origin = (PUBLIC_BACKEND_URL || '').replace(/\/$/, '');
 
-console.log("origin:", origin);
 export const authClient = createAuthClient({
 	baseURL: `${origin}/auth`,
 	trustedOrigins: [origin],
