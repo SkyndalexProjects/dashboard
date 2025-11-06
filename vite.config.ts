4b174df128c14c7222b5de38e491a0ca6c0aab9d
KorrumzPL <korrumzprivate@gmail.com>
2025-11-06 15:57:15 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	const target = env.BACKEND_URL;
+    const backendDev = env.BACKEND_URL || 'http://127.0.0.1:4173';
 
 	return {
 		plugins: [sveltekit()],
@@ -12,7 +13,12 @@ export default defineConfig(({ mode }) => {
                 '/api': {
                     target: 'http://localhost:4173',
                     changeOrigin: true,
-                    secure: false
+                    secure: false,
+                    configure: (proxy) => {
+                        proxy.on('proxyReq', (_proxyReq, req) => {
+                            req.headers.host = new URL(backendDev).host;
+                        });
+                    }
                 }
             }
         }
